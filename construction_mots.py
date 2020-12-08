9d5fac6c455b1491594ee9c03987f8d2d809d00f
MrIanDoe <52634689+MrIanDoe@users.noreply.github.com>
2020-12-08 19:03:04 +0100

    Verification code
    Modification de la fonction mot_jouable et suppression du superflue

--- construction_mots.py
+++ construction_mots.py
@@ -4,11 +4,10 @@ def generer_dico():
     for line in f:
         dico_final.append(line.rstrip())
     return dico_final
-print(generer_dico())
+#print(generer_dico())
 
 def mot_jouable(mot,lettres):
-    liste_lettre=list(lettres)
-    i=0
+    """liste_lettre = list(lettres)
     compte=0
     possible=False
     for i in liste_lettre:
@@ -18,7 +17,25 @@ def mot_jouable(mot,lettres):
     if compte>=len(mot):
         possible=True
     return possible
+    """
+    d1 = dict()
+    for c in mot:
+        d1[c.upper()] = d1.get(c.upper(),0) + 1
+    #print(d1)
+
+    d2 = dict()
+    for c in lettres:
+        d2[c.upper()] = d2.get(c.upper(),0) + 1
+    #print(d2)
+    possible = True
+    for key in d1:
+        #print(key,d2[key])
+        if (key not in d2) or d1[key] > d2[key]:
+            possible = False
     
+
+    return possible
+
 def mots_jouables(liste_mots,lettres):
     v=[]
     for i in liste_mots:
@@ -27,7 +44,12 @@ def mots_jouables(liste_mots,lettres):
             v.append(i)
     return v
 
-liste=['mordo','ourir','courrir','mourir','valhala']
-l1=['o','u','R','r','','c','i']  
+liste=generer_dico()
+l1=['o','u','R','r','r','r','','c','i','M']  
 print ('main:',l1)
 print('possibilités:',mots_jouables(liste,l1))
+print ('main:',l1)
+
+"""l1=['u','R','r','r','','c','i']  
+print(mot_jouable("ourir",l1))"""
+
