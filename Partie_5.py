d1d3710d700e081f2fce78cc2cdcb6c315e48e53
nincapable <74893257+nincapable@users.noreply.github.com>
2020-12-09 22:51:02 +0100

    Create Partie_5.py
    C'est super pas fini, ca marche pas encore. mais je le mets la on sait jamais. TKT je gere.

---
+++ Partie_5.py
@@ -0,0 +1,115 @@
+cases_MT = [[0,0],[0,7],[0,14],[7,0],[7,14],[14,0],[14,7],[14,14]]
+cases_MD = [[1,1],[1,13],[2,2],[2,12],[3,3],[3,11],[4,4],[4,10],[7,7],[10,4],[10,10],[11,3],[11,11],[12,2],[12,12],[13,1],[13,13]]
+cases_LT = [[1,5],[1,9],[5,1],[5,5],[5,9],[5,13],[9,1],[9,5],[9,9],[9,13],[13,5],[13,9]]
+cases_LD = [[0,3],[0,11],[2,6],[2,8],[3,0],[3,7],[3,14],[6,2],[6,6],[6,8],[6,12],[7,3],[7,11],[8,2],[8,6],[8,8],[8,12],[11,0],[11,7],[11,14],[12,6],[12,8],[14,3],[14,11]]
+
+def board():
+    board=[]
+    for i in range (15):
+        line=[]
+        for j in range(15):
+            line.append("x")
+        board.append(line)
+    return board
+
+def affichageBoard(board):
+    for line in board:
+        for elem in line:
+            print(elem, end=" ")
+        print()
+
+def changeBoard(lettre, position, board):
+    board[position[0]].pop(position[1])
+    board[position[0]].insert(position[1],lettre)
+    return board
+
+def init_bonus(board):
+    for position in cases_MT:
+        changeBoard('MT', position, board)
+    for position in cases_MD:
+        changeBoard("MD", position, board)
+    for position in cases_LT:
+        changeBoard("LT", position, board)
+    for position in cases_LD:
+        changeBoard("LD", position, board)
+
+#COMPARE LE PLATEAU AVANT L'APPLICATION DES BONUS ET APRES
+b = board()
+affichageBoard(b)
+print()
+init_bonus(b)
+affichageBoard(b)
+
+
+def lire_coords(board):
+    x=int(input("colonne: "))
+    y=int(input("ligne: "))
+    coord=board[y][x]
+    while coord not in "xMTMDLTLD":
+        x=int(input("colonne: "))
+        y=int(input("ligne: "))
+        print(board[y][x])
+    return x, y
+
+def tester_placement(board,x,y,direction,mot):
+    initialisation = board[y][x] 
+    lettres=list(mot)
+    print(lettres)
+    liste_lettres=["t","h"]
+    lettres_necessaires=[]
+    
+    if str(direction.lower()) == "horizontale" and len(mot)<=15-x:
+        for i in range (len(mot)):
+            print(i)
+            print(board[y][i+x])
+
+            if board[y][i+x] not in 'xMTMDLTLD':
+                liste_lettres=liste_lettre.append(board[y][i+x])
+        ##if liste_lettres in lettres or liste_lettres == ["s"]:
+          ##  lettres_necessaires=lettres-liste_lettres
+        for l in liste_lettres:
+            lettres.remove(l)
+
+    elif direction.lower() == "verticale" and len(mot)<=15-y:
+        for i in range (len(mot)):
+            if board[i+y][x] not in 'xMTMDLTLD':
+                liste_lettres=liste_lettre.append(board[i+y][x])
+        ##if liste_lettres in lettres or liste_lettres == ["s"]:
+          ##  lettres_necessaires=lettres-liste_lettres
+        for l in liste_lettres:
+            lettres.remove(l)
+    else:
+        lettres=[-1]
+    
+    return lettres
+
+##def placer_mot(board,main,mot,x,y,direction):
+  ##  lettres_necessaires=tester_placement(board,x,y,direction,mot)
+    ##possible=False
+    ##if lettres_necessaires in main:
+##        possible=True
+  ##      if direction.lower()=="horizontale":
+    ##        for k in len(mot):
+      ##          for i in lettres_necessaires:
+        ##            if i in main : 
+          ##              main.pop(i)
+            ##        board[y][x+k]=i
+##        if direction.lower()=="horizontale":
+  ##          for k in len(mot):
+    ##            for i in lettres_necessaires:
+      ##              if i in main : 
+        ##                main.pop(i)
+          ##          board[y+k][x]=i
+    ##return possible
+
+hand=['t','h','e','i','e','r','e']
+i=0
+while i <2:
+    mot=input('mot: ')
+    x,y=lire_coords(b)
+    direction=input('horizontale/verticale: ')
+    ##placer_mot(b,hand,mot,x,y,direction)
+    print(b)
+    print(hand)
+    print(tester_placement(b,x,y,direction,mot))
+    i=i+1
