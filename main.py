261fb0da1b7fee44dc908ca5b159afcfbbcbf3f4
MrIanDoe <52634689+MrIanDoe@users.noreply.github.com>
2020-12-13 19:21:34 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+from pioche import init_dico, init_pioche
+from plateau import init_bonus, board
+from construction_mots import generer_dico
+from tour_de_jeux import initialisation, tour_joueur
+import os
+
+def affichage():
+    os.system('cls')
+    art = """   _____ __________  ___    ____  ____  __    ______
+  / ___// ____/ __ \/   |  / __ )/ __ )/ /   / ____/
+  \__ \/ /   / /_/ / /| | / __  / __  / /   / __/   
+ ___/ / /___/ _, _/ ___ |/ /_/ / /_/ / /___/ /___   
+/____/\____/_/ |_/_/  |_/_____/_____/_____/_____/   
+                                                    """
+
+    print('#---------------------------------------------------#\n')
+    print(art)
+    print('\n#---------------------------------------------------#\n')
+
+def menu():
+    action = 0
+    POSSIBILITE = ["1", "2", "3"]
+    while action not in POSSIBILITE:
+        affichage()
+        print("1 - JOUER | 2 - CHARGER | 3 - QUITTER")
+        action = input("> ")
+
+    if action == '1':
+        #  INITIALISATION VARIABLES 
+        lettres = init_dico()
+        sac = init_pioche(lettres)
+        b = board()
+        liste_mots = generer_dico()
+        #----------------------------------
+        joueurs = initialisation(sac)
+        ordre = 0
+        init_bonus(b)
+        sac = ['R','O','B','I','N','E','T']
+        tour_joueur(b, joueurs[ordre]["main"], sac, joueurs, ordre, liste_mots, True)
+
+
+menu()
