b861573d583ea19795b56fcccbfc4ad78038f53a
MrIanDoe <52634689+MrIanDoe@users.noreply.github.com>
2020-12-14 21:48:15 +0100

    Final update

--- plateau.py
+++ plateau.py
@@ -1,5 +1,6 @@
 import copy
 
+#Créer un plateau de 15*15
 def board():
     board=[]
     for i in range(15):
@@ -9,24 +10,30 @@ def board():
         board.append(line)
     return board
 
+#Formate l'affichage du plateau pour le rendre plus esthétique, ne le modifie pas
 def affichageBoard(board):
     affBoard = copy.deepcopy(board)
-    coord = [" ","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"]
+    coord = [" "," ","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"]
+    vide = ["_"*50]
     affBoard.insert(0,coord)
-    
-    for i in range(1,len(affBoard)):
-        affBoard[i].insert(0,str(i))
+    affBoard.insert(1,vide)
+
+    for i in range(2,len(affBoard)):
+        affBoard[i].insert(0,str(i-1))
+        affBoard[i].insert(1,'|')
  
     for line in affBoard:
         for elem in line:
             print(elem.rjust(2), end=" ")
         print()
 
+#Permet de placer une lettre sur une case du plateau
 def changeBoard(lettre, position, board):
     board[position[0]].pop(position[1])
     board[position[0]].insert(position[1],lettre)
     return board
 
+#Place les bonus sur le plateau
 def init_bonus(board):
     cases_MT = [[0,0],[0,7],[0,14],[7,0],[7,14],[14,0],[14,7],[14,14]]
     cases_MD = [[1,1],[1,13],[2,2],[2,12],[3,3],[3,11],[4,4],[4,10],[7,7],[10,4],[10,10],[11,3],[11,11],[12,2],[12,12],[13,1],[13,13]]
@@ -34,16 +41,10 @@ def init_bonus(board):
     cases_LD = [[0,3],[0,11],[2,6],[2,8],[3,0],[3,7],[3,14],[6,2],[6,6],[6,8],[6,12],[7,3],[7,11],[8,2],[8,6],[8,8],[8,12],[11,0],[11,7],[11,14],[12,6],[12,8],[14,3],[14,11]]
 
     for position in cases_MT:
-        changeBoard('MT', position, board)
+        changeBoard('*', position, board) #mot triple
     for position in cases_MD:
-        changeBoard("MD", position, board)
+        changeBoard("^", position, board) #mot double
     for position in cases_LT:
-        changeBoard("LT", position, board)
+        changeBoard("-", position, board) #lettre triple
     for position in cases_LD:
-        changeBoard("LD", position, board)
-
-#COMPARE LE PLATEAU AVANT L'APPLICATION DES BONUS ET APRES
-"""b = board()
-init_bonus(b)
-affichageBoard(b)"""
-
+        changeBoard("+", position, board) #lettre double
