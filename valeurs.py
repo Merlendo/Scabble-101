d55de89dc8b8bf7432a5eaced106b8d7b197dc00
nincapable <74893257+nincapable@users.noreply.github.com>
2020-12-14 10:10:07 +0100

    Update valeurs.py
    >Les valeurs avec le scrabble et les bonus

--- valeurs.py
+++ valeurs.py
@@ -8,6 +8,22 @@ def valeur_mot(mot,dico):
         
     return valeur
 
+def valeur_mot_bonus(mot,dico,values_bonus,scrabble):
+    valeur=0
+    i=0
+    for c in mot.upper():
+
+        valeur=valeur + dico[c]["val"]*values_bonus[1][i]
+        i=i+1
+    if values_bonus[0][0]!=0:
+        valeur=valeur*values_bonus[0][0]
+    elif values_bonus[0][1]!=0:
+        valeur=valeur*values_bonus[0][1]
+    if scrabble:
+        valeur=valeur+50
+    
+    return valeur
+
 
 def meilleur_mot(liste_mots_possibles,lettres,dico):
     mots = mots_jouables(liste_mots_possibles,lettres)
@@ -21,6 +37,7 @@ def meilleur_mot(liste_mots_possibles,lettres,dico):
         if valeur_mot(mot,dico) == valeur_mot(meilleurMot,dico):
             meilleursMots.append(mot)
     return meilleursMots
+
 
 """lettres = init_dico()
 dico=generer_dico()
